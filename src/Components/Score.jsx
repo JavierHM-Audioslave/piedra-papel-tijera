a0aa2fdfeef5ed35399ee9f1f6e834e7f386b821
JavierHM-Audioslave <javorengero2000@yahoo.com.ar>
2022-01-20 13:12:37 -0300

    Decresing more the score height

--- src/Components/Score.jsx
+++ src/Components/Score.jsx
@@ -17,14 +17,14 @@ const Score = ({result}) => {
             <Container>
                 <Row className="my-5 justify-content-center">
                     <Col xs={12} lg={6}>
-                        <div id="score" className="d-flex justify-content-between p-4 border border-2 border-light rounded-3">
+                        <div id="score" className="d-flex justify-content-between p-3 border border-2 border-light rounded-3">
                             <div className="d-flex flex-column justify-content-center align-items-center text-light">
                                 <h5>ROCK</h5>
                                 <h5>PAPER</h5>
                                 <h5>SCISSORS</h5>
                             </div>
-                            <div className="bg-light px-5 py-1 d-flex flex-column justify-content-center align-items-center">
-                                <h3>Score</h3>
+                            <div className="bg-light px-5 d-flex flex-column justify-content-center align-items-center">
+                                <span style={{fontSize: "1.5em"}}>Score</span>
                                 <h5 className="text-muted mb-0 fw-bold scoreNumber">{scoreSelector}</h5>
                             </div>
                         </div>
